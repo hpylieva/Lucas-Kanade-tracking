73f20f8f6cfd9eeab39271a5f8e60886a60f9aa4
pylyeva@gmail.com <t27sj4ph81>
2018-06-27 12:05:24 +0300

    Template matching done.

--- funtions.py
+++ funtions.py
@@ -5,6 +5,17 @@ from matplotlib import pyplot as plt
 
 
 def plot_match_result(img, match_result, top_left, bottom_right, method_name):
+    """
+    Plot results of template matching.
+    Images are automatically scaled in plt.imshow() like:
+    img = (img - np.min(img))/(np.max(img) - np.min(img))
+    :param img:
+    :param match_result:
+    :param top_left:
+    :param bottom_right:
+    :param method_name:
+    :return:
+    """
     cv.rectangle(img, top_left, bottom_right, 255, 2)
     plt.subplot(121),plt.imshow(match_result,cmap = 'gray')
     plt.title('Matching Result'), plt.xticks([]), plt.yticks([])
@@ -13,24 +24,34 @@ def plot_match_result(img, match_result, top_left, bottom_right, method_name):
     plt.suptitle(method_name)
     plt.show()
 
-
-def TM_SAD(	image, templ):
+def template_matching(image, template, method = 'SSD'):
     """
-
-    :param image: image in the greyscale
-    :param templ:
-    :return:
+    Computes template matching.
+    :param image: image in the grey scale
+    :param templ: template in grey scale
+    :param method: chosen appearance similarity function for convolution
+    :return: Result of comparison the template to cutted aread from image in each point.
+    The minimum pr maximum value of this result will be loaction of top left corner of image we search
     """
-    # minSAD = sys.float_info.max
-    # bestRow, bestCol = [0,0]
     im_height, im_width = image.shape
-    t_height, t_width = templ.shape
+    t_height, t_width = template.shape
 
     res = np.zeros((im_height - t_height, im_width - t_width))
+    image = np.array(image, dtype = 'float32')
     # loop through the search image
-    for w in range(im_height - t_height):
-        for h in range(im_width - t_width):
-            x,y = [0,0]
-            res[w,h] = np.sum(np.absolute(image[w:(w+t_height), h:(h+t_width)] - templ))
-
+    for h in range(im_height - t_height):
+        for w in range(im_width - t_width):
+            patch = image[h:(h+t_height), w:(w+t_width)]
+            if method=='SSD':
+                res[h, w] = np.sum(np.power(patch - template,2))
+            elif method=='NCC':
+                res[h, w] = np.sum(np.multiply(patch, template))
+                # np.sum(np.multiply(image[w:(w+t_height), h:(h+t_width)], templ))
+                res[h, w] /= np.sqrt(np.sum(np.square(patch)) * np.sum(np.square(template)))
+            elif method=='SAD':
+                res[h, w] = np.sum(np.absolute(patch- template))
     return res
+
+
+def LK(image):
+    pass
--- template-matching.py
+++ template-matching.py
@@ -4,36 +4,52 @@ from matplotlib import pyplot as plt
 import funtions
 
 img = cv.imread('baby.jpg',0)
-img2 = img.copy()
-template = cv.imread('tail.jpg',0)
+img_copy = img.copy()
+template = cv.imread('babyface.jpg',0)
+template_copy = template.copy()
 h, w = template.shape
-# All the 6 methods for comparison in a list
+targetPoint = (162, 72)
+
+run_opencv_methods = False
+if run_opencv_methods:
+    methods = [
+        'cv.TM_SQDIFF',
+        'cv.TM_CCORR_NORMED'
+    ]
+
+    for meth in methods:
+        img = img_copy.copy()
+        method = eval(meth)
+        # apply template Matching
+        res = cv.matchTemplate(img,template,method)
+        min_val, max_val, min_loc, max_loc = cv.minMaxLoc(res)
+        # if the method is TM_SQDIFF or TM_SQDIFF_NORMED, take minimum
+        if method in [cv.TM_SQDIFF, cv.TM_SQDIFF_NORMED]:
+            top_left = min_loc
+        else:
+            top_left = max_loc
+        print(top_left)
+        bottom_right = (top_left[0] + w, top_left[1] + h)
+        funtions.plot_match_result(img, res, top_left, bottom_right, meth)
+
+
 methods = [
-'cv.TM_SQDIFF',
-'cv.TM_CCORR_NORMED'
+    'SSD',
+    'NCC',
+    'SAD'
 ]
 
-for meth in methods:
-    img = img2.copy()
-    method = eval(meth)
-    # apply template Matching
-    res = cv.matchTemplate(img,template,method)
+for method in methods:
+    img = img_copy.copy()
+    template = template_copy.copy()
+    res = funtions.template_matching(img, template, method)
+    # res = (1 - (res - np.min(res))/(np.max(res) - np.min(res)))*255
+    # res = np.array(res, dtype = "uint8")
     min_val, max_val, min_loc, max_loc = cv.minMaxLoc(res)
-    # if the method is TM_SQDIFF or TM_SQDIFF_NORMED, take minimum
-    if method in [cv.TM_SQDIFF, cv.TM_SQDIFF_NORMED]:
+    if method in ['SSD', 'SAD']:
         top_left = min_loc
     else:
         top_left = max_loc
+    # print("Top left point of area where template match", top_left)
     bottom_right = (top_left[0] + w, top_left[1] + h)
-   # funtions.plot_match_result(img, res, top_left, bottom_right, meth)
-
-# separately run for SAD method as it has custom implementation
-img = img2.copy()
-# Apply template Matching
-res = funtions.TM_SAD(img,template)
-min_val, max_val, min_loc, max_loc = cv.minMaxLoc(res)
-top_left = max_loc
-#np.unravel_index(np.argmax(res), res.shape)
-print(top_left)
-bottom_right = (top_left[0] + w, top_left[1] + h)
-funtions.plot_match_result(img, res, top_left, bottom_right, "TM_SAD")
+    funtions.plot_match_result(img, res, top_left, bottom_right, str(method))
